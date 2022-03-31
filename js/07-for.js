9c2ab700ab48d81811c29f32feaea2d8bf1bc597
julijajarmale <julija.jarmale@gmail.com>
2022-03-31 13:15:27 +0300

    ciklai

---
+++ js/07-for.js
@@ -0,0 +1,33 @@
+/* const pazymiai = [10, 2, 8, 4, 6, 8, 10, 1];
+
+const pazymiuKiekis = pazymiai.length;
+let pazymiuSuma = 0;
+
+pazymiuSuma += pazymiai[0];
+pazymiuSuma += pazymiai[1];
+pazymiuSuma += pazymiai[2];
+pazymiuSuma += pazymiai[3];
+pazymiuSuma += pazymiai[4];
+pazymiuSuma += pazymiai[5];
+pazymiuSuma += pazymiai[6];
+pazymiuSuma += pazymiai[7];
+
+const vidurkis = pazymiuSuma / pazymiuKiekis;
+console.log(vidurkis);
+*/
+/*
+Ciklas = LOOP: for, while, for-in, foreach...
+for- visu ciklu tevas */
+
+const pazymiai = [10, 2, 8, 4, 6, 8];
+
+console.log ('START');
+for (let i=0; i<5; i++) {
+    console.log('Pirmas', i);
+
+}
+console.log ('END');
+for (let i=0; i<7; i++) {
+    console.log('Antras', i);
+}
+console.log ('FINISH');
